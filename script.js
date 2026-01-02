a1a2f78ca01f190f6bfecd89e2193091f5687633
Rogate25 <rogaterogate41@gmail.com>
2026-01-02 11:22:25 +0700

    adding save button

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+let prev = document.getElementById("entries-p")
 let number = document.getElementById("num");
 let count= 0;
 
 function increment(){
-    count = count + 1;
-    number.innerText = count
+    count += 1;
+    number.textContent = count
+}
+
+function save(){
+    let countstr = count + " - " 
+    prev.textContent += countstr
+
+    number.textContent = 0;
+    count = 0;
 }
